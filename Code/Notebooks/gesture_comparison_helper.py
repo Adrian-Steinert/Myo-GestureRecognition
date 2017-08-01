9711f3825d9c313fc21dc0c93c8b85424f48c8b2
Adrian Steinert <adriansteinert@gmx.de>
2017-08-01 07:30:58 +0200

    Searching a bug in accelerometer feature extraction process

--- Code/Notebooks/gesture_comparison_helper.py
+++ Code/Notebooks/gesture_comparison_helper.py
@@ -5,17 +5,20 @@ import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
-from glob import glob
+from scipy.fftpack import fft
 
 
 def acc_print(data, ylim_range=[-1.5, 1.5]):
     f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', sharey='row')
 
     ax1.set_title('Accelerometer - x')
+    ax1.grid(color='grey', linestyle='-.')
     ax1.set_ylim(ylim_range)
     ax2.set_title('Accelerometer - y')
+    ax2.grid(color='grey', linestyle='-.')
     ax2.set_ylim(ylim_range)
     ax3.set_title('Accelerometer - z')
+    ax3.grid(color='grey', linestyle='-.')
     ax3.set_ylim(ylim_range)
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(data))))
@@ -35,20 +38,28 @@ def acc_print(data, ylim_range=[-1.5, 1.5]):
     f.legend(handles, labels, 'center right')
 
 
-def emg_print(data, ylim_range=[-128, 128]):
+def emg_print(data, ylim_range=[-150, 150]):
     f, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4, 2, sharex='col', sharey='row')
 
     ax1.set_title('EMG 1')
+    ax1.grid(color='grey', linestyle='-.')
     ax2.set_title('EMG 2')
+    ax2.grid(color='grey', linestyle='-.')
     ax2.set_ylim(ylim_range)
     ax3.set_title('EMG 3')
+    ax3.grid(color='grey', linestyle='-.')
     ax4.set_title('EMG 4')
+    ax4.grid(color='grey', linestyle='-.')
     ax4.set_ylim(ylim_range)
     ax5.set_title('EMG 5')
+    ax5.grid(color='grey', linestyle='-.')
     ax6.set_title('EMG 6')
+    ax6.grid(color='grey', linestyle='-.')
     ax6.set_ylim(ylim_range)
     ax7.set_title('EMG 7')
+    ax7.grid(color='grey', linestyle='-.')
     ax8.set_title('EMG 8')
+    ax8.grid(color='grey', linestyle='-.')
     ax8.set_ylim(ylim_range)
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(data))))
@@ -73,14 +84,17 @@ def emg_print(data, ylim_range=[-128, 128]):
     f.legend(handles, labels, 'center right')
 
 
-def gyro_print(data, ylim_range=[-150, 150]):
+def gyro_print(data, ylim_range=[-250, 250]):
     f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', sharey='row')
 
     ax1.set_title('Gyroscope - x')
+    ax1.grid(color='grey', linestyle='-.')
     ax1.set_ylim(ylim_range)
     ax2.set_title('Gyroscope - y')
+    ax2.grid(color='grey', linestyle='-.')
     ax2.set_ylim(ylim_range)
     ax3.set_title('Gyroscope - z')
+    ax3.grid(color='grey', linestyle='-.')
     ax3.set_ylim(ylim_range)
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(data))))
@@ -104,12 +118,16 @@ def orientation_print(data, ylim_range=[-1.5, 1.5]):
     f, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex='col', sharey='row')
 
     ax1.set_title('Orientation - x')
+    ax1.grid(color='grey', linestyle='-.')
     ax1.set_ylim(ylim_range)
     ax2.set_title('Orientation - y')
+    ax2.grid(color='grey', linestyle='-.')
     ax2.set_ylim(ylim_range)
     ax3.set_title('Orientation - z')
+    ax3.grid(color='grey', linestyle='-.')
     ax3.set_ylim(ylim_range)
     ax4.set_title('Orientation - w')
+    ax4.grid(color='grey', linestyle='-.')
     ax4.set_ylim(ylim_range)
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(data))))
@@ -134,10 +152,13 @@ def orientation_euler_print(data, ylim_range=[-5, 5]):
     f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', sharey='row')
 
     ax1.set_title('Orientation Euler - roll')
+    ax1.grid(color='grey', linestyle='-.')
     ax1.set_ylim(ylim_range)
     ax2.set_title('Orientation Euler - pitch')
+    ax2.grid(color='grey', linestyle='-.')
     ax2.set_ylim(ylim_range)
     ax3.set_title('Orientation Euler - yaw')
+    ax3.grid(color='grey', linestyle='-.')
     ax3.set_ylim(ylim_range)
 
     colors = iter(cm.rainbow(np.linspace(0, 1, len(data))))
@@ -161,16 +182,125 @@ def all_print(data):
     emg_print(data)
     gyro_print(data)
     orientation_print(data)
-    orientation_euler_print(data)
+    # orientation_euler_print(data)
 
 
 def load_gestures(path):
-    # converted_data = glob('../Data/converted/*')
-
     loaded_data = []
 
     for element in path:
         loaded_data.append(pickle.load(open(element, 'rb')))
     return loaded_data
 
+# sensor_data is a list with data of sensor data axes
+def split_in_frames(sensor_data, frame_number):
+    '''
+    every two adjunct sequences make a frame
+    => input data has one segment more than frames
+    => frame length is two times segment lenght
+    '''
+    data_length = len(sensor_data[0])
+    segment_count = frame_number + 1
+    segment_length = data_length // segment_count
+    cut_off = data_length % segment_count
+    frame_length = 2 * segment_length
 
+    segmented_data = []
+    for sensor_axis in sensor_data:
+        if cut_off > 0:
+            filtered_sensor_axis = sensor_axis[:-cut_off]
+        else:
+            filtered_sensor_axis = sensor_axis[:]
+
+        segmented_axis = []
+        for segment in range(0, len(filtered_sensor_axis), segment_length):
+            step_size = segment + frame_length
+            segmented_axis.append(filtered_sensor_axis[segment:step_size])
+
+        segmented_data.append(segmented_axis)
+
+    return segmented_data
+
+def create_acc_feature_vector(frame_list):
+    if frame_list is None:
+        print('frame_list was None in create create_acc_feature_vector()')
+        return None
+
+    acc_feature_vector = []
+    x_axis = frame_list[0]
+    y_axis = frame_list[1]
+    z_axis = frame_list[2]
+
+    for x_segment, y_segment, z_segment in zip(x_axis, y_axis, z_axis):
+        acc_feature_vector.extend(create_frequency_domain_features(x_segment, y_segment, z_segment))
+        acc_feature_vector.extend(create_time_domain_features(x_segment, y_segment, z_segment))
+
+    return acc_feature_vector
+
+
+def create_frequency_domain_features(x_segment, y_segment, z_segment):
+    frequency_domain_features = []
+
+    # first feature: mean
+    x_fft = fft(x_segment)
+    y_fft = fft(y_segment)
+    z_fft = fft(z_segment)
+    x_mean = x_fft[0]
+    y_mean = y_fft[0]
+    z_mean = z_fft[0]
+    frequency_domain_features.extend([x_mean, y_mean, z_mean])
+
+    # second feature: energy
+    x_energy = calculate_energy(x_fft[1:])
+    y_energy = calculate_energy(y_fft[1:])
+    z_energy = calculate_energy(z_fft[1:])
+    frequency_domain_features.extend([x_energy, y_energy, z_energy])
+
+    # third feature: entropy
+    x_entropy = calculate_entropy(x_fft, x_fft[1:])
+    y_entropy = calculate_entropy(y_fft, y_fft[1:])
+    z_entropy = calculate_entropy(z_fft, z_fft[1:])
+    frequency_domain_features.extend([x_entropy, y_entropy, z_entropy])
+
+    return frequency_domain_features
+
+
+def calculate_energy(fft_no_mean_segment):
+    energy = np.divide(np.sum(np.square(np.absolute(fft_no_mean_segment))), len(fft_no_mean_segment + 1))
+    return energy
+
+
+def calculate_entropy(fft_segment, fft_no_mean_segment):
+    probability = np.divide(np.absolute(fft_segment), np.sum(np.absolute(fft_no_mean_segment)))
+    entropy = np.sum(np.multiply(probability, np.log2(np.divide(1, probability))))
+    return entropy
+
+
+def create_time_domain_features(x_segment, y_segment, z_segment):
+    time_domain_features = []
+
+    # fourth feature: standard deviation
+    x_std_dev = np.std(x_segment)
+    y_std_dev = np.std(y_segment)
+    z_std_dev = np.std(z_segment)
+    time_domain_features.extend([x_std_dev, y_std_dev, z_std_dev])
+
+    # fith feature: axis correlation
+    x_y_correlation = calculate_axis_correlation(x_segment, y_segment)
+    x_z_correlation = calculate_axis_correlation(x_segment, z_segment)
+    y_z_correlation = calculate_axis_correlation(y_segment, z_segment)
+    time_domain_features.extend([x_y_correlation, x_z_correlation, y_z_correlation])
+
+    return time_domain_features
+
+
+def calculate_axis_correlation(axis_1, axis_2):
+    different_axis = np.divide(np.sum(np.abs(np.multiply(axis_1, axis_2))), len(axis_1))
+    same_axis_1 = np.divide(np.sum(np.abs(np.multiply(axis_1, axis_1))), len(axis_1))
+    same_axis_2 = np.divide(np.sum(np.abs(np.multiply(axis_2, axis_2))), len(axis_1))
+
+    correlation_enumerator = np.subtract(different_axis, np.multiply(np.mean(axis_1), np.mean(axis_2)))
+    correlation_denominator = np.multiply(np.sqrt(np.subtract(same_axis_1, np.square(np.mean(axis_1)))),
+                                          np.sqrt(np.subtract(same_axis_2, np.square(np.mean(axis_2)))))
+    correlation = np.divide(correlation_enumerator, correlation_denominator)
+    return correlation
--- Code/Tools/CSVconverter.py
+++ Code/Tools/CSVconverter.py
@@ -17,9 +17,6 @@ from multiprocessing import Pool
 rawPath = os.path.join(os.path.dirname(sys.path[0]), 'Data', 'raw')
 convertedPath = os.path.join(os.path.dirname(sys.path[0]), 'Data', 'converted')
 
-gesture_dict_lock = threading.Lock()
-interpolated_dict_lock = threading.Lock()
-
 
 def acquire_acc_or_gyro(path_to_acc_or_gyro_file, gesture_dict, is_acc=True):
     data_dict = {'timestamps': [], 'x': [], 'y': [], 'z': []}
@@ -33,12 +30,11 @@ def acquire_acc_or_gyro(path_to_acc_or_gyro_file, gesture_dict, is_acc=True):
             data_dict['x'].append(float(row[1]))
             data_dict['y'].append(float(row[2]))
             data_dict['z'].append(float(row[3]))
-    # with gesture_dict_lock:
+
     if is_acc:
         gesture_dict['accelerometer'] = data_dict
     else:
         gesture_dict['gyro'] = data_dict
-    # return data_dict
 
 
 def acquire_emg(path_to_emg_file, gesture_dict):
@@ -59,9 +55,7 @@ def acquire_emg(path_to_emg_file, gesture_dict):
             emg_dict['7'].append(int(row[7]))
             emg_dict['8'].append(int(row[8]))
 
-    # with gesture_dict_lock:
     gesture_dict['emg'] = emg_dict
-    # return emg_dict
 
 
 def acquire_orientation(path_to_orientation_file, gesture_dict):
@@ -77,9 +71,7 @@ def acquire_orientation(path_to_orientation_file, gesture_dict):
             ori_dict['z'].append(float(row[3]))
             ori_dict['w'].append(float(row[4]))
 
-    # with gesture_dict_lock:
     gesture_dict['orientation'] = ori_dict
-    # return ori_dict
 
 
 def acquire_orientation_euler(path_to_orientationEuler_file, gesture_dict):
@@ -95,11 +87,11 @@ def acquire_orientation_euler(path_to_orientationEuler_file, gesture_dict):
             ori_euler_dict['roll'].append(float(row[2]))
             ori_euler_dict['yaw'].append(float(row[3]))
 
-    # with gesture_dict_lock:
     gesture_dict['orientationEuler'] = ori_euler_dict
 
 
 def acquire_data(user, gesture, gesture_dict):
+    print('Creating pickle {}/{}... '.format(user, gesture), flush=True)
     recordedData = glob(os.path.join(rawPath, user, gesture) + '\*')
 
     # sort by last modified date
@@ -115,39 +107,36 @@ def acquire_data(user, gesture, gesture_dict):
         gesture_dict['datetime'] = datetime.fromtimestamp(os.path.getctime(gesture_chunk[0]))
         gesture_dict['performed_by'] = user
 
-        acquire_acc_or_gyro(gesture_chunk[0], gesture_dict, is_acc=True)
-        acquire_emg(gesture_chunk[1], gesture_dict)
-        acquire_acc_or_gyro(gesture_chunk[2], gesture_dict, is_acc=False)
-        acquire_orientation_euler(gesture_chunk[3], gesture_dict)
-        acquire_orientation(gesture_chunk[4], gesture_dict)
+        # acquire_acc_or_gyro(gesture_chunk[0], gesture_dict, is_acc=True)
+        # acquire_emg(gesture_chunk[1], gesture_dict)
+        # acquire_acc_or_gyro(gesture_chunk[2], gesture_dict, is_acc=False)
+        # acquire_orientation_euler(gesture_chunk[3], gesture_dict)
+        # acquire_orientation(gesture_chunk[4], gesture_dict)
 
-        # t_acc = threading.Thread(target=acquire_acc_or_gyro, args=(gesture_chunk[0], gesture_dict, True), daemon=True)
-        # t_acc.start()
-        # t_emg = threading.Thread(target=acquire_emg, args=(gesture_chunk[1], gesture_dict), daemon=True)
-        # t_emg.start()
-        # t_gyro = threading.Thread(target=acquire_acc_or_gyro, args=(gesture_chunk[2], gesture_dict, False), daemon=True)
-        # t_gyro.start()
+        t_acc = threading.Thread(target=acquire_acc_or_gyro, args=(gesture_chunk[0], gesture_dict, True), daemon=True)
+        t_acc.start()
+        t_emg = threading.Thread(target=acquire_emg, args=(gesture_chunk[1], gesture_dict), daemon=True)
+        t_emg.start()
+        t_gyro = threading.Thread(target=acquire_acc_or_gyro, args=(gesture_chunk[2], gesture_dict, False), daemon=True)
+        t_gyro.start()
         # t_oriEuler = threading.Thread(target=acquire_orientation_euler, args=(gesture_chunk[3], gesture_dict), daemon=True)
         # t_oriEuler.start()
-        # t_ori = threading.Thread(target=acquire_orientation, args=(gesture_chunk[4], gesture_dict), daemon=True)
-        # t_ori.start()
+        t_ori = threading.Thread(target=acquire_orientation, args=(gesture_chunk[4], gesture_dict), daemon=True)
+        t_ori.start()
 
-        # t_acc.join()
-        # t_emg.join()
-        # t_gyro.join()
+        t_acc.join()
+        t_emg.join()
+        t_gyro.join()
         # t_oriEuler.join()
-        # t_ori.join()
-
-        start, end = find_gesture_start_end(gesture_dict)
-        # interpolated_dict = interpolate_data(start, end, 1_000, gesture_dict) # interpolated values every millisecond
-
+        t_ori.join()
 
         # save dictionary as pickle to disk
-        savePath = os.path.join(convertedPath, user, gesture)
+        savePath = os.path.join(convertedPath, gesture_dict['performed_by'], gesture_dict['gesture'])
         os.makedirs(savePath, exist_ok=True)
         # extract timestamp from csv file name
         timestamp = re.sub(r'.*\\[a-z_]+', '', gesture_chunk[0]).strip('.csv')
-        pickle.dump(gesture_dict, open(os.path.join(savePath, gesture + timestamp) + '.p', 'wb'))
+        pickle.dump(gesture_dict, open(os.path.join(savePath, gesture_dict['gesture'] + timestamp) + '.p', 'wb'))
+        # print(interpolated_dict)
 
 
 def eliminate_duplicates(list_with_duplicates):
@@ -166,13 +155,19 @@ def eliminate_duplicates(list_with_duplicates):
 
 
 # timestep_size is an integer with unit microseconds
-def interpolate_data(start, end, timestep_size, gesture_dict):
+#
+# interpolation from scipy.interpolate.interp1d return numpy ndarrays with a single element, i cast this to float before
+# appending to a list
+def interpolate_data(gesture_dict, interpolated_dict, timestep_size):
+    # start = time.time()
+    gesture_start, gesture_end = find_gesture_start_end(gesture_dict)
+    # print('Find start/end:   {}'.format(time.time() - start))
+    # print('-'*20)
     # prepare interpolated dict
-    interpolated_dict = {}
     interpolated_dict['gesture'] = gesture_dict['gesture']
     interpolated_dict['datetime'] = gesture_dict['datetime']
     interpolated_dict['performed_by'] = gesture_dict['performed_by']
-    interpolated_dict['timestamps'] = list(range(start, end + 1, timestep_size)) # end shall be included thus + 1
+    interpolated_dict['timestamps'] = list(range(gesture_start, gesture_end + 1, timestep_size)) # end shall be included thus + 1
 
     # get references to internal dicts
     acc_dict = gesture_dict['accelerometer']
@@ -180,87 +175,6 @@ def interpolate_data(start, end, timestep_size, gesture_dict):
     gyro_dict = gesture_dict['gyro']
     ori_dict = gesture_dict['orientation']
     ori_euler_dict = gesture_dict['orientationEuler']
-
-    # interpolate and add results
-    interpolated_dict['accelerometer'] = interpolate_acc_or_gyro_or_oriEuler(interpolated_dict['timestamps'], acc_dict)
-    interpolated_dict['emg'] = interpolate_emg(interpolated_dict['timestamps'], emg_dict)
-    interpolated_dict['gyro'] = interpolate_acc_or_gyro_or_oriEuler(interpolated_dict['timestamps'], gyro_dict)
-    interpolated_dict['orientation'] = interpolate_orientation(interpolated_dict['timestamps'], ori_dict)
-    interpolated_dict['orientationEuler'] = interpolate_acc_or_gyro_or_oriEuler(interpolated_dict['timestamps'],
-                                                                          ori_euler_dict, is_oriEuler=True)
-
-    return interpolated_dict
-
-
-def interpolate_acc_or_gyro_or_oriEuler(timestamp_list, a_g_oE_dict, is_oriEuler=False):
-    a_g_oE_dict['timestamps'] = eliminate_duplicates(a_g_oE_dict['timestamps'])
-
-    if is_oriEuler:
-        result_dict = {'roll': [], 'pitch': [], 'yaw': []}
-
-        roll_interpolate = interp1d(a_g_oE_dict['timestamps'], a_g_oE_dict['roll'], kind='cubic', fill_value='extrapolate')
-        pitch_interpolate = interp1d(a_g_oE_dict['timestamps'], a_g_oE_dict['pitch'], kind='cubic', fill_value='extrapolate')
-        yaw_interpolate = interp1d(a_g_oE_dict['timestamps'], a_g_oE_dict['yaw'], kind='cubic', fill_value='extrapolate')
-
-        [result_dict['roll'].append(roll_interpolate(timestamp)) for timestamp in timestamp_list]
-        [result_dict['pitch'].append(pitch_interpolate(timestamp)) for timestamp in timestamp_list]
-        [result_dict['yaw'].append(yaw_interpolate(timestamp)) for timestamp in timestamp_list]
-
-    else:
-        result_dict = {'x': [], 'y': [], 'z': []}
-        ao_x_interpolate = interp1d(a_g_oE_dict['timestamps'], a_g_oE_dict['x'], kind='cubic', fill_value='extrapolate')
-        ao_y_interpolate = interp1d(a_g_oE_dict['timestamps'], a_g_oE_dict['y'], kind='cubic', fill_value='extrapolate')
-        ao_z_interpolate = interp1d(a_g_oE_dict['timestamps'], a_g_oE_dict['z'], kind='cubic', fill_value='extrapolate')
-
-        [result_dict['x'].append(ao_x_interpolate(timestamp)) for timestamp in timestamp_list]
-        [result_dict['y'].append(ao_y_interpolate(timestamp)) for timestamp in timestamp_list]
-        [result_dict['z'].append(ao_z_interpolate(timestamp)) for timestamp in timestamp_list]
-
-    return result_dict
-
-
-def interpolate_emg(timestamp_list, emg_dict):
-    result_dict = {'1': [], '2': [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': []}
-
-    emg_dict['timestamps'] = eliminate_duplicates(emg_dict['timestamps'])
-
-    emg1_interpolate = interp1d(emg_dict['timestamps'], emg_dict['1'], kind='cubic', fill_value='extrapolate')
-    emg2_interpolate = interp1d(emg_dict['timestamps'], emg_dict['2'], kind='cubic', fill_value='extrapolate')
-    emg3_interpolate = interp1d(emg_dict['timestamps'], emg_dict['3'], kind='cubic', fill_value='extrapolate')
-    emg4_interpolate = interp1d(emg_dict['timestamps'], emg_dict['4'], kind='cubic', fill_value='extrapolate')
-    emg5_interpolate = interp1d(emg_dict['timestamps'], emg_dict['5'], kind='cubic', fill_value='extrapolate')
-    emg6_interpolate = interp1d(emg_dict['timestamps'], emg_dict['6'], kind='cubic', fill_value='extrapolate')
-    emg7_interpolate = interp1d(emg_dict['timestamps'], emg_dict['7'], kind='cubic', fill_value='extrapolate')
-    emg8_interpolate = interp1d(emg_dict['timestamps'], emg_dict['8'], kind='cubic', fill_value='extrapolate')
-
-    [result_dict['1'].append(emg1_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['2'].append(emg2_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['3'].append(emg3_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['4'].append(emg4_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['5'].append(emg5_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['6'].append(emg6_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['7'].append(emg7_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['8'].append(emg8_interpolate(timestamp)) for timestamp in timestamp_list]
-
-    return result_dict
-
-
-def interpolate_orientation(timestamp_list, ori_dict):
-    result_dict = {'x': [], 'y': [], 'z': [], 'w': []}
-
-    ori_dict['timestamps'] = eliminate_duplicates(ori_dict['timestamps'])
-
-    ori_x_interpolate = interp1d(ori_dict['timestamps'], ori_dict['x'], kind='cubic', fill_value='extrapolate')
-    ori_y_interpolate = interp1d(ori_dict['timestamps'], ori_dict['y'], kind='cubic', fill_value='extrapolate')
-    ori_z_interpolate = interp1d(ori_dict['timestamps'], ori_dict['z'], kind='cubic', fill_value='extrapolate')
-    ori_w_interpolate = interp1d(ori_dict['timestamps'], ori_dict['w'], kind='cubic', fill_value='extrapolate')
-
-    [result_dict['x'].append(ori_x_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['y'].append(ori_y_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['z'].append(ori_z_interpolate(timestamp)) for timestamp in timestamp_list]
-    [result_dict['w'].append(ori_w_interpolate(timestamp)) for timestamp in timestamp_list]
-
-    return result_dict
 
 
 def find_gesture_start_end(gesture_dict):
@@ -278,20 +192,20 @@ def find_gesture_start_end(gesture_dict):
     sorted_list = sorted([acc_timestamp_start, acc_timestamp_end, emg_timestamp_start, emg_timestamp_end,
                          gyro_timestamp_start, gyro_timestamp_end, ori_timestamp_start, ori_timestamp_end])
 
-    start = math.floor(sorted_list[0] / CONVERT_MILLI_MICRO) * CONVERT_MILLI_MICRO
-    end = math.ceil(sorted_list[-1] / CONVERT_MILLI_MICRO) * CONVERT_MILLI_MICRO
+    start = int(math.floor(sorted_list[0] / CONVERT_MILLI_MICRO) * CONVERT_MILLI_MICRO)
+    end = int(math.ceil(sorted_list[-1] / CONVERT_MILLI_MICRO) * CONVERT_MILLI_MICRO)
 
     # start and end are returned in microseconds
     return start, end
 
 
-def main():
+def get_parameters():
     rawUserPath = glob(rawPath + '\*')
     rawUsers = [re.sub(r'.*\\', '', user) for user in rawUserPath]
-    # rawUsers = ['flo']
+    # rawUsers = ['adrian']
     # print(rawUsers)
 
-    thread_list = []
+    parameter_list = []
 
     for user in rawUsers:
         rawGesturePath = glob(os.path.join(rawPath, user) + '\*')
@@ -311,24 +225,24 @@ def main():
             print('Converted gestures are up-to-date.')
             break
 
-        print('Converting gestures for {}... '.format(user), end='\n', flush=True)
+        # print('Converting gestures for {}... '.format(user), end='\n', flush=True)
 
         for gesture in rawGestures:
             gesture_dict = {}
-            # print('Creating pickles {}/{}... '.format(user, gesture), end='', flush=True)
             # acquire_data(user, gesture, gesture_dict)
-            # print('DONE')
 
-            t = threading.Thread(target=acquire_data, args=(user, gesture, gesture_dict), daemon=True)
-            thread_list.append(t)
-            t.start()
-        # print('DONE')
+            parameter_list.append((user, gesture, gesture_dict))
+    return parameter_list
 
-    for thread in thread_list:
-        thread.join()
 
+def main():
+    params = get_parameters()
+
+    with Pool() as p:
+        p.starmap(acquire_data, params)
 
 if __name__ == '__main__':
     starttime = time.time()
     main()
+    # get_parameters()
     print('CSVconverter: {}'.format(time.time()-starttime))
